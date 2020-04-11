7cf2046cd9e200f8ee2d7f65b0b2c1b9d9ad9b53
Mulenga Bowa <mulengabowa53@gmail.com>
2020-04-11 15:11:08 +0200

    added arguement checks

--- lib/bluzelle/swarm/client.rb
+++ lib/bluzelle/swarm/client.rb
@@ -3,10 +3,18 @@ module Bluzelle
         class Client
             attr_reader :address, :mnemonic, :uuid, :chain_id, :endpoint
 
+            # Initializes a new client object
+
+            # @param options [Hash]
+            # @return [Bluzelle::Swarm::Client]
             def initialize(options = {})
+                if options[:address].nil? || options[:mnemonic].nil?
+                    raise ArgumentError
+                end
+                
                 @address = options[:address]
                 @mnemonic = options[:mnemonic]
-                @uuid = options[:uuid]
+                @uuid = options[:uuid] || @address
                 @chain_id = options[:chain_id] || 'bluzelle'
                 @endpoint = options[:endpoint] || 'http://localhost:1317'
             end
--- spec/bluzelle/swarm/client_spec.rb
+++ spec/bluzelle/swarm/client_spec.rb
@@ -1,23 +1,43 @@
 require 'spec_helper'
 
 RSpec.describe Bluzelle::Swarm::Client do
-    it 'has default chain_id' do
-        @client = Bluzelle::Swarm::Client.new(
-            address: '',
-            mnemonic: '',
-            uuid: '',
-            endpoint: ''
+    let(:client) {
+        Bluzelle::Swarm::Client.new(
+            address: 'bluzelle1xhz23a58mku7ch3hx8f9hrx6he6gyujq57y3kp',
+            mnemonic: 'volcano arrest ceiling physical concert sunset absent hungry tobacco canal census era pretty car code crunch inside behind afraid express giraffe reflect stadium luxury',
+            endpoint: "http://localhost:1317",
+            chain_id: "bluzelle",
+            uuid: '20fc19d4-7c9d-4b5c-9578-8cedd756e0ea'
         )
-        expect(@client.chain_id).to eq('bluzelle')
-    end
+    }
 
-    it 'has default endpoint' do
-        @client = Bluzelle::Swarm::Client.new(
-            address: '',
-            mnemonic: '',
-            uuid: '',
-            chain_id: ''
-        )
-        expect(@client.endpoint).to eq('http://localhost:1317')
+    describe '#initialize' do
+        it 'with default chain_id, endpoint and uuid' do
+            @client = Bluzelle::Swarm::Client.new(
+                address: client.address,
+                mnemonic: client.mnemonic,
+            )
+            expect(@client.chain_id).to eq(client.chain_id)
+            expect(@client.endpoint).to eq(client.endpoint)
+            expect(@client.uuid).to eq(client.address)
+        end
+    
+        it 'without address throws error' do
+            expect{ Bluzelle::Swarm::Client.new(
+                mnemonic: client.mnemonic,
+                endpoint: client.endpoint,
+                chain_id: client.chain_id,
+                uuid: client.uuid
+            ) }.to raise_error(ArgumentError)
+        end
+
+        it 'without mnemonic throws error' do
+            expect{ Bluzelle::Swarm::Client.new(
+                address: client.address,
+                endpoint: client.endpoint,
+                chain_id: client.chain_id,
+                uuid: client.uuid
+            )}.to raise_error(ArgumentError)
+        end
     end
 end
