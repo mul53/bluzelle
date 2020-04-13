e4b2625931066ae20486046e0928268e49d2d3cf
Mulenga Bowa <mulengabowa53@gmail.com>
2020-04-13 15:40:25 +0200

    utils module refactor

--- lib/bluzelle/utils.rb
+++ lib/bluzelle/utils.rb
@@ -6,26 +6,22 @@ require 'openssl'
 
 module Bluzelle
     module Utils
+        PREFIX = 'bluzelle'
+        PATH = "m/44'/118'/0'/0/0"
+
         module_function
 
         def get_ec_private_key(mnemonic)
-            seed_hex = BipMnemonic.to_seed(mnemonic: mnemonic)
-            master = MoneyTree::Master.new(seed_hex: seed_hex)
-            child = master.node_for_path("m/44'/118'/0'/0/0")
-            ec_pair = Bitcoin::Key.new(child.private_key.to_hex, nil, { compressed: false })
+            seed = seed_from_mnemonic(mnemonic)
+            node = bip32_from_seed(seed)
+            child = node.node_for_path(PATH)
+            ec_pair = create_ec_pair(child.private_key.to_hex)
             ec_pair.priv
         end
 
         def get_ec_public_key_from_priv(priv)
-            group = OpenSSL::PKey::EC::Group.new('secp256k1')
-            key = OpenSSL::PKey::EC.new(group)
-
-            key.private_key = OpenSSL::BN.new(priv, 16)
-            key.public_key = group.generator.mul(key.private_key)
-
-            public_key = key.public_key
-            public_key.group.point_conversion_form = :compressed
-            public_key.to_hex.rjust(66, '0')
+            key = open_key(priv)
+            compressed_pub_key(key)
         end
 
         def validate_address(address, mnemonic)
@@ -36,11 +32,57 @@ module Bluzelle
         end
 
         def get_address(pub_key)
-            hash = Digest::RMD160.digest(
-                Digest::SHA256.digest([pub_key].pack('H*'))
-            )
-            word = Bitcoin::Bech32.convert_bits(hash.bytes, from_bits: 8, to_bits: 5, pad: false)
-            Bitcoin::Bech32.encode('bluzelle', word)
+            hash = rmd_160_digest(sha_256_digest([pub_key].pack('H*')))
+            word = bech32_convert_bits(to_bytes(hash))
+            bech32_encode(PREFIX, word)
+        end
+
+        def to_bytes(obj)
+            obj.bytes
+        end
+
+        def rmd_160_digest(hex)
+            Digest::RMD160.digest hex
+        end
+
+        def sha_256_digest(hex)
+            Digest::SHA256.digest hex
+        end
+
+        def bech32_encode(prefix, word)
+            Bitcoin::Bech32.encode(prefix, word)
+        end
+
+        def bech32_convert_bits(bytes, from_bits: 8, to_bits: 5, pad: false)
+            Bitcoin::Bech32.convert_bits(bytes, from_bits: from_bits, to_bits: to_bits, pad: pad)
+        end
+
+        def seed_from_mnemonic(mnemonic)
+            BipMnemonic.to_seed(mnemonic: mnemonic)
+        end
+
+        def open_key(priv)
+            group = OpenSSL::PKey::EC::Group.new('secp256k1')
+            key = OpenSSL::PKey::EC.new(group)
+
+            key.private_key = OpenSSL::BN.new(priv, 16)
+            key.public_key = group.generator.mul(key.private_key)
+
+            key
+        end
+
+        def compressed_pub_key(key)
+            public_key = key.public_key
+            public_key.group.point_conversion_form = :compressed
+            public_key.to_hex.rjust(66, '0')
+        end
+
+        def bip32_from_seed(seed)
+            MoneyTree::Master.new(seed_hex: seed)
+        end
+
+        def create_ec_pair(private_key)
+            Bitcoin::Key.new(private_key, nil, { compressed: false })
         end
     end
 end
